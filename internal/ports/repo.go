8a4a83ceacf3faaaa6974fe95df5c78e7d89a252
akagiyuu <khanghuynh0245@gmail.com>
2026-03-02 22:53:19 +0700

    feat(repo): implement in memory repo

--- internal/ports/repo.go
+++ internal/ports/repo.go
@@ -1,1 +1,13 @@
 package ports
+
+import (
+	"context"
+
+	"github.com/akagiyuu/chaos-adventure-api/internal/domain"
+	"github.com/google/uuid"
+)
+
+type Repository interface {
+	CreateAccount(ctx context.Context, username string, password string) (uuid.UUID, error)
+	GetAccount(ctx context.Context, id uuid.UUID) (*domain.Account, error)
+}
