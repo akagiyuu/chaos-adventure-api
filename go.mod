8319c305ea1c64b195a81a47c6ff1d9ef00271f9
akagiyuu <khanghuynh0245@gmail.com>
2026-03-03 14:04:00 +0700

    feat(usecase/auth): add token related functions

--- go.mod
+++ go.mod
@@ -8,7 +8,23 @@ require (
 )
 
 require (
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.4.1 // indirect
+	github.com/goccy/go-json v0.10.5 // indirect
+	github.com/lestrrat-go/blackmagic v1.0.4 // indirect
+	github.com/lestrrat-go/dsig v1.0.0 // indirect
+	github.com/lestrrat-go/dsig-secp256k1 v1.0.0 // indirect
+	github.com/lestrrat-go/httpcc v1.0.1 // indirect
+	github.com/lestrrat-go/httprc/v3 v3.0.4 // indirect
+	github.com/lestrrat-go/option/v2 v2.0.0 // indirect
+	github.com/segmentio/asm v1.2.1 // indirect
+	github.com/valyala/fastjson v1.6.10 // indirect
+	golang.org/x/crypto v0.48.0 // indirect
+	golang.org/x/sys v0.41.0 // indirect
+)
+
+require (
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
-	golang.org/x/text v0.29.0 // indirect
+	github.com/lestrrat-go/jwx/v3 v3.0.13
+	golang.org/x/text v0.34.0 // indirect
 )
--- internal/usecase/auth.go
+++ internal/usecase/auth.go
@@ -1,7 +1,80 @@
 package usecase
 
-import "github.com/akagiyuu/chaos-adventure-api/internal/ports"
+import (
+	"fmt"
+	"go/printer"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/lestrrat-go/jwx/v3/jwa"
+	"github.com/lestrrat-go/jwx/v3/jwk"
+	"github.com/lestrrat-go/jwx/v3/jwt"
+
+	"github.com/akagiyuu/chaos-adventure-api/internal/config"
+	"github.com/akagiyuu/chaos-adventure-api/internal/ports"
+)
 
 type Auth struct {
-	Repo ports.Repository
+	privkey   jwk.Key
+	pubkey    jwk.Key
+	expiredIn time.Duration
+	repo      ports.Repository
+}
+
+func NewAuth(
+	cfg *config.Config,
+	repo ports.Repository,
+) (*Auth, error) {
+	privkey, err := jwk.ParseKey(cfg.JWTSecret)
+	if err != nil {
+		return nil, err
+	}
+
+	pubkey, err := jwk.PublicKeyOf(privkey)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Auth{
+		privkey:   privkey,
+		pubkey:    pubkey,
+		expiredIn: time.Duration(cfg.JWTExpiredIn) * time.Hour,
+		repo:      repo,
+	}, nil
+}
+
+func (u *Auth) CreateToken(id uuid.UUID) ([]byte, error) {
+	token, err := jwt.NewBuilder().
+		Subject(id.String()).
+		Expiration(time.Now().Add(u.expiredIn)).
+		Build()
+	if err != nil {
+		return nil, err
+	}
+
+	signed, err := jwt.Sign(token, jwt.WithKey(jwa.RS256(), u.privkey))
+	if err != nil {
+		return nil, err
+	}
+
+	return signed, err
+}
+
+func (u *Auth) ParseToken(raw []byte) (uuid.UUID, error) {
+	token, err := jwt.Parse(raw, jwt.WithKey(jwa.RS256(), u.pubkey))
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	sub, exist := token.Subject()
+	if !exist {
+		return uuid.Nil, fmt.Errorf("Token contain no subject")
+	}
+
+	id, err := uuid.Parse(sub)
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	return id, nil
 }
