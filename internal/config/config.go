879e9ea31edb48c9a4038ff5eb91dda245bdba4a
akagiyuu <khanghuynh0245@gmail.com>
2026-03-03 13:41:05 +0700

    feat: add config

---
+++ internal/config/config.go
@@ -0,0 +1,10 @@
+package config
+
+type Config struct {
+	Port int `env:"PORT" envDefault:"3000"`
+
+	DatabaseURL string `env:"DATBASE_URL"`
+
+	JWTSecret    []byte `env:"JWT_SECRET" envDefault:"secret"`
+	JWTExpiredIn int    `env:"JWT_EXPIRED_IN" envDefault:"24"` // hour
+}
