bb94ae64bbe5d4ae12be00c58a17a6a600b269fe
akagiyuu <khanghuynh0245@gmail.com>
2026-03-04 17:55:00 +0700

    feat(transports/http): add basic handlers

--- internal/transports/http/handler.go
+++ internal/transports/http/handler.go
@@ -1,1 +1,31 @@
 package http
+
+import (
+	"net/http"
+
+	"github.com/akagiyuu/chaos-adventure-api/internal/config"
+	"github.com/akagiyuu/chaos-adventure-api/internal/usecase"
+	"github.com/go-fuego/fuego"
+	httpSwagger "github.com/swaggo/http-swagger/v2"
+)
+
+type Handler struct {
+	Config *config.Config
+	Auth   usecase.Auth
+}
+
+func (h *Handler) RegisterRoutes(s *fuego.Server) {
+	fuego.Get(s, "/", h.Ping)
+}
+
+func (h *Handler) OpenAPI(specURL string) http.Handler {
+	return httpSwagger.Handler(
+		httpSwagger.Layout(httpSwagger.StandaloneLayout),
+		httpSwagger.PersistAuthorization(true),
+		httpSwagger.URL(specURL),
+	)
+}
+
+func (h *Handler) Ping(c fuego.ContextNoBody) (string, error) {
+	return "pong", nil
+}
--- internal/transports/http/server.go
+++ internal/transports/http/server.go
@@ -1,1 +1,37 @@
 package http
+
+import (
+	"fmt"
+
+	"github.com/getkin/kin-openapi/openapi3"
+	"github.com/go-fuego/fuego"
+)
+
+func (h *Handler) BuildServer() *fuego.Server {
+	s := fuego.NewServer(
+		fuego.WithAddr(fmt.Sprintf(":%d", h.Config.Port)),
+		fuego.WithEngineOptions(
+			fuego.WithOpenAPIConfig(fuego.OpenAPIConfig{
+				UIHandler:            h.OpenAPI,
+				DisableDefaultServer: true,
+				DisableMessages:      true,
+				Info: &openapi3.Info{
+					Title:       "General Service",
+					Description: "General Service",
+				},
+			}),
+		),
+		fuego.WithSecurity(openapi3.SecuritySchemes{
+			"bearerAuth": &openapi3.SecuritySchemeRef{
+				Value: openapi3.NewSecurityScheme().
+					WithType("http").
+					WithScheme("bearer").
+					WithBearerFormat("JWT").
+					WithDescription("Enter your JWT token in the format: Bearer <token>"),
+			},
+		}),
+	)
+	h.RegisterRoutes(s)
+
+	return s
+}
