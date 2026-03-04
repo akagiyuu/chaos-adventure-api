34365ce9981aa261092c631cffaa9ca351eaf8f3
akagiyuu <khanghuynh0245@gmail.com>
2026-03-04 19:10:19 +0700

    feat(ports/repo): add rank related

--- internal/ports/repo.go
+++ internal/ports/repo.go
@@ -11,4 +11,7 @@ type Repository interface {
 	CreateAccount(ctx context.Context, data domain.RegisterData) (uuid.UUID, error)
 	GetAccount(ctx context.Context, id uuid.UUID) (*domain.Account, error)
 	GetAccountByUsername(ctx context.Context, username string) (*domain.Account, error)
+
+	CreateRank(ctx context.Context, id uuid.UUID, time float32) error
+	GetAllRank(ctx context.Context) ([]domain.Rank, error)
 }
