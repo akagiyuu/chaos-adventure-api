b94c25c3ffb9b7c486ed0d72642465d8e8fef4b8
akagiyuu <khanghuynh0245@gmail.com>
2026-03-04 19:19:09 +0700

    feat(database): add table records

---
+++ migration/000002_create_record.up.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS records(
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    account_id uuid NOT NULL REFERENCES accounts(id),
+    time real NOT NULL,
+    created_at timestamptz NOT NULL DEFAULT now(),
+);
