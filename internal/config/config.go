0b35b9950df5846e52b764f451635bdc62c3c4d8
akagiyuu <khanghuynh0245@gmail.com>
2026-03-04 18:28:37 +0700

    refactor(config): remove unused jwt secret env

--- internal/config/config.go
+++ internal/config/config.go
@@ -5,6 +5,5 @@ type Config struct {
 
 	DatabaseURL string `env:"DATABASE_URL"`
 
-	JWTSecret    string `env:"JWT_SECRET" envDefault:"secret"`
 	JWTExpiredIn int    `env:"JWT_EXPIRED_IN" envDefault:"24"` // hour
 }
