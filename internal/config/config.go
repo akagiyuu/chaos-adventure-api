90442d4b8c6da28d208bb342794a89401202fb39
akagiyuu <khanghuynh0245@gmail.com>
2026-03-04 18:27:46 +0700

    fix(database): correct env name

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,7 +3,7 @@ package config
 type Config struct {
 	Port int `env:"PORT" envDefault:"3000"`
 
-	DatabaseURL string `env:"DATBASE_URL"`
+	DatabaseURL string `env:"DATABASE_URL"`
 
 	JWTSecret    string `env:"JWT_SECRET" envDefault:"secret"`
 	JWTExpiredIn int    `env:"JWT_EXPIRED_IN" envDefault:"24"` // hour
